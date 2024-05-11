57cce5ace02adab905f1007172e8704722fd07c1
kevin colmenares <kevincolmenaresm@gmail.com>
2024-05-11 18:21:13 -0400

    chore: Add middleware for session authentication

---
+++ middleware.ts
@@ -0,0 +1,52 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { veryfyToken } from "./lib/example";
+import next from "next";
+
+export async function middleware(request: NextRequest) {
+  const session = cookies().get("session");
+
+  const authArray = ["/login", "/register", "/forgot-password"];
+
+  if (session) {
+    const token = session.value;
+
+    const data = await veryfyToken(token);
+
+    if (!data && request.nextUrl.pathname !== "/login") {
+      return NextResponse.redirect(request.nextUrl.origin + "/login");
+    }
+
+    const res = await fetch(process.env.USER_SERVICE_URL as string, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res && request.nextUrl.pathname !== "/login") {
+      return NextResponse.redirect(request.nextUrl.origin + "/login");
+    }
+
+    if (authArray.includes(request.nextUrl.pathname)) {
+      return NextResponse.redirect(request.nextUrl.origin + "/");
+    }
+  } else if (!session && !authArray.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(request.nextUrl.origin + "/login");
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     *
+     * Or the ones that match "url.extension" to exclude images and such
+     */
+    "/((?!_next/static|_next/image|.*\\..*).*)",
+  ],
+};
