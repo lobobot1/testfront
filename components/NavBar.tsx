7300483072a7a4f218ac7aff2e99aa275a16f0fa
kevin colmenares <kevincolmenaresm@gmail.com>
2024-05-12 14:44:41 -0400

    feat: Add NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { MdLogout } from "react-icons/md";
+
+const NavBar = () => {
+  return (
+    <nav className="flex items-center justify-between bg-blue-500 p-3">
+      <div className="flex items-center flex-shrink-0 text-white">
+        <a href="/">
+          <span className="font-semibold text-xl tracking-tight">
+            Product List
+          </span>
+        </a>
+      </div>
+      <div className="flex items-center">
+        <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white">
+          Logout
+          <MdLogout className="inline-block ml-2" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
