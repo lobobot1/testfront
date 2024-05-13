79ae768784d651b88c609abf819dc3b69cdae5ae
kevin colmenares <kevincolmenaresm@gmail.com>
2024-05-13 19:23:46 -0400

    refactor: Update session module to use cookies() function for setting session token

--- lib/session.ts
+++ lib/session.ts
@@ -1,21 +1,5 @@
 "use server";
-
-import { cookies } from "next/headers";
 import * as jose from "jose";
-
-const maxAge = 60 * 60 * 24 * 7 * 4; // 4 weeks
-
-export async function register(token: string) {
-  try {
-    cookies().set("session", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      expires: maxAge,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
 
 export async function veryfyToken(token: string) {
   try {
